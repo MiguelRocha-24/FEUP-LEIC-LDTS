10abb3f869df25e2142bab9868acb6d4aa6975ea
MiguelRocha-24 <miguelmirandarocha@gmail.com>
2025-11-29 15:36:59 +0000

    Movable Lane Testing

---
+++ src/test/java/feup2526/ldts/t02g03/model/MovableLaneTest.java
@@ -0,0 +1,31 @@
+package feup2526.ldts.t02g03.model;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MovableLaneTest{
+    private class TestMovableLane extends MovableLane {
+        public TestMovableLane(int row, Direction direction, int speed) {
+            super(row, direction, speed);
+        }
+    }
+
+    @Test
+    void testBuilder(){
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {new TestMovableLane(-1, Direction.LEFT, 1);});
+        assertTrue(thrown.getMessage().contains("Row"));
+        IllegalArgumentException thrown2 = assertThrows(IllegalArgumentException.class, () -> {new TestMovableLane(1, null, -1);});
+        assertTrue(thrown2.getMessage().contains("Lane"));
+        IllegalArgumentException thrown3 = assertThrows(IllegalArgumentException.class, () -> {new TestMovableLane(1, Direction.UP, -1);});
+        assertTrue(thrown3.getMessage().contains("Lane"));
+        IllegalArgumentException thrown4 = assertThrows(IllegalArgumentException.class, () -> {new TestMovableLane(1, Direction.LEFT, 0);});
+        assertTrue(thrown4.getMessage().contains("Speed"));
+    }
+
+    @Test
+    void testGetters(){
+        TestMovableLane lane = new TestMovableLane(1, Direction.LEFT, 1);
+        assertEquals(1, lane.getRow());
+        assertEquals(Direction.LEFT, lane.getDirection());
+        assertEquals(1, lane.getSpeed());
+    }
+}
