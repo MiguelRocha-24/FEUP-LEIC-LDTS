cfc9b5436d1faf1b209ced03901b42a7e71597e8
MiguelRocha-24 <miguelmirandarocha@gmail.com>
2025-12-12 14:56:46 +0000

    Sprites Efficiency Improved - Removal of transparent Background from car and tree

--- src/main/java/feup2526/ldts/t02g03/view/game/LogViewer.java
+++ src/main/java/feup2526/ldts/t02g03/view/game/LogViewer.java
@@ -6,13 +6,13 @@ import feup2526.ldts.t02g03.view.GUIImage;
 
 public class LogViewer extends SpriteViewer<Log> {
     public LogViewer() {
-        super("docs/images/sprites/log2.png");
+        super("docs/images/sprites/log.png");
     }
 
     @Override
     public void draw(GUI gui, Log log, int tileSize) {
         GUIImage sprite = getSprite(gui);
         drawSprite(gui, sprite, (int) (log.getPosition().getX() * tileSize),
-                (int) (log.getPosition().getY() * tileSize));
+                (int) (log.getPosition().getY() * tileSize)+3);
     }
 }
