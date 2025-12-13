b8322347860536e785f918e2f4ce196edecbadf5
MiguelRocha-24 <miguelmirandarocha@gmail.com>
2025-12-13 19:50:09 +0000

    Added Chicken Direction And Death Sprites, Implemented chicken Direction

--- src/main/java/feup2526/ldts/t02g03/view/game/PlayerViewer.java
+++ src/main/java/feup2526/ldts/t02g03/view/game/PlayerViewer.java
@@ -1,29 +1,33 @@
 package feup2526.ldts.t02g03.view.game;
 
+import feup2526.ldts.t02g03.model.game.Direction;
 import feup2526.ldts.t02g03.model.game.Player;
 import feup2526.ldts.t02g03.view.GUI;
 import feup2526.ldts.t02g03.view.GUIImage;
 
 public class PlayerViewer extends SpriteViewer<Player> {
-    private String skinName;
+    private String leftSprite;
+    private String rightSprite;
+    private Direction lastHorizontalDirection = Direction.RIGHT;
 
     public PlayerViewer() {
-        super("docs/images/sprites/chicken.png");
-        this.skinName = "chicken";
+        super("docs/images/sprites/chickenRight.png");
+        setSkinName("chicken");
     }
 
     public void setSkinName(String skinName) {
-        this.skinName = skinName;
-    }
-
-    @Override
-    protected GUIImage getSprite(GUI gui) {
-        return super.getSprite(gui, "docs/images/sprites/" + skinName + ".png");
+        this.leftSprite = "docs/images/sprites/" + skinName + "Left.png";
+        this.rightSprite = "docs/images/sprites/" + skinName + "Right.png";
     }
 
     @Override
     public void draw(GUI gui, Player player, int tileSize, int yPos) {
-        GUIImage sprite = getSprite(gui);
+        Direction direction = player.getDirection();
+        if (direction == Direction.LEFT || direction == Direction.RIGHT) {
+            lastHorizontalDirection = direction;
+        }
+        String path = (lastHorizontalDirection == Direction.LEFT) ? leftSprite : rightSprite;
+        GUIImage sprite = getSprite(gui, path);
         drawSprite(gui, sprite, (int) (player.getPosition().getX() * tileSize), yPos - 1);
     }
 }
