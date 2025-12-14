d2792be6684dc456ead418980ad61104e4599ed6
MiguelRocha-24 <miguelmirandarocha@gmail.com>
2025-12-14 12:41:32 +0000

    Updated Main menu viewer and controller to allow removal of users

--- src/main/java/feup2526/ldts/t02g03/controller/menu/MenuController.java
+++ src/main/java/feup2526/ldts/t02g03/controller/menu/MenuController.java
@@ -5,6 +5,12 @@ import feup2526.ldts.t02g03.controller.Controller;
 import feup2526.ldts.t02g03.model.menu.Menu;
 import com.googlecode.lanterna.input.KeyStroke;
 import java.io.IOException;
+import feup2526.ldts.t02g03.states.NewUserState;
+import feup2526.ldts.t02g03.states.RemoveUserState;
+import feup2526.ldts.t02g03.model.menu.RemoveUser;
+import feup2526.ldts.t02g03.model.menu.User;
+import feup2526.ldts.t02g03.states.ShopState;
+import feup2526.ldts.t02g03.model.menu.Shop;
 
 public class MenuController extends Controller<Menu> {
     public MenuController(Menu menu) {
@@ -30,11 +36,13 @@ public class MenuController extends Controller<Menu> {
 
                     if (selectedIndex == numUsers) {
                         // "New User" selected
-                        game.setState(new feup2526.ldts.t02g03.states.NewUserState(getModel()));
+                        game.setState(new NewUserState(getModel()));
+                    } else if (selectedIndex == numUsers + 1) {
+                        if (!getModel().getUserManager().getUsers().isEmpty()) {
+                            game.setState(new RemoveUserState(new RemoveUser(getModel())));
+                        }
                     } else {
-                        // Existing user selected
-                        feup2526.ldts.t02g03.model.menu.User selectedUser = getModel().getUserManager().getUsers()
-                                .get(selectedIndex);
+                        User selectedUser = getModel().getUserManager().getUsers().get(selectedIndex);
                         game.setCurrentUser(selectedUser);
                         getModel().setCurrentUser(selectedUser);
                         getModel().setUserListActive(false);
@@ -64,7 +72,7 @@ public class MenuController extends Controller<Menu> {
                     if (getModel().isSelected("Change User"))
                         getModel().setUserListActive(true);
                     if (getModel().isSelected("Shop"))
-                        game.setState(new feup2526.ldts.t02g03.states.ShopState(new feup2526.ldts.t02g03.model.menu.Shop(getModel().getCurrentUser())));
+                        game.setState(new ShopState(new Shop(getModel().getCurrentUser())));
                     break;
                 case EOF:
                     game.setState(null);
