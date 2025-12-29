4bfdc28e8834ed1c6fed91d63198a1a29920d100
MiguelRocha-24 <miguelmirandarocha@gmail.com>
2025-12-29 12:11:29 +0000

    removal of unused imports in states package

--- src/test/java/feup2526/ldts/t02g03/states/StateTest.java
+++ src/test/java/feup2526/ldts/t02g03/states/StateTest.java
@@ -5,16 +5,14 @@ import feup2526.ldts.t02g03.controller.Controller;
 import feup2526.ldts.t02g03.view.GUI;
 import feup2526.ldts.t02g03.view.Viewer;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class StateTest {
 
     @Test
+    @SuppressWarnings("unchecked")
     void testStep() throws IOException {
         // Create a stub concrete State
         Controller<Object> mockController = mock(Controller.class);
